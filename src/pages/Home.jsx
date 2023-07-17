50e45c2c6d3070fb8e40b3ddb00e5220199a6bf5
Amin <pahlavanmohammadamin@gmail.com>
2023-07-17 14:10:54 +0330

    home page progress

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,20 @@ import Layout from "./Layout";
 const Home = () => {
   return (
       <Layout>
-        <h1 className="text-3xl bg-pink-400">Home page</h1>
+       <div className="flex flex-col items-center align-center  gap-4 sm:flex-row mx-auto max-w-[1024px] px-6 " >
+        <div className="hero">
+          <h1 className="hero-title">The Beast Platform for CarRental</h1>
+          <p>Ease of doing a car rental sarely and reliably. Of course at a low price.</p>
+          <button className="btn">Rental car</button>
+          <img src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/model_detail/countach/menu/menu_countach_lpi_01.png" alt="lamborgini" className="w-10/12 h-[90%] max-h-44 mx-auto" />
+        </div>
+        <div className="hero">
+          <h1 className="hero-title">The Beast Platform for CarRental</h1>
+          <p>Ease of doing a car rental sarely and reliably. Of course at a low price.</p>
+          <button className="btn">Rental car</button>
+          <img src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/model_detail/countach/menu/menu_countach_lpi_01.png" alt="lamborgini" className="w-10/12 h-[90%] max-h-44 mx-auto" />
+        </div>
+       </div>
       </Layout>
       );
 } 
